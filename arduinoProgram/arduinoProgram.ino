8f066a3d74342a48fd91842706f0c822e7e3529d
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-07 22:03:35 +0100

    Test with delay and stuff

--- arduinoProgram/arduinoProgram.ino
+++ arduinoProgram/arduinoProgram.ino
@@ -1,11 +1,36 @@
 void setup() {
   Serial.begin(57600); // opens serial port, sets data rate to 57600 bps
-  Serial.setTimeout(10);
+  //Serial.setTimeout(1000);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-  // read the incoming string:
-  String incomingString = Serial.readString();
-  // prints the received data
-  Serial.println(incomingString);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  
+  if (Serial.available() > 0) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    // read the incoming string:
+    String incomingString = Serial.readString();
+    // prints the received data
+    Serial.println(incomingString);
+  }
 }
--- main.cpp
+++ main.cpp
@@ -8,14 +8,11 @@ int main(){
     ArduinoSerialIO arduino("/dev/ttyACM0");
     
     while(1){
-        auto t1 = std::chrono::high_resolution_clock::now();
         arduino.writeString("HELLO WORLD");
         arduino.readUntilNewline();
         std::cout << arduino.getDataString();
         arduino.clearDataVect();
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
-        std::cout << "Time taken: " << ms_int.count() << "ms\n";
     }
+    
     return 0;
 }
