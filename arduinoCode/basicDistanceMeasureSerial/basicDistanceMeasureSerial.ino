71c90ccd4710f98d3e971cc73a46d0db5cda708b
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-07 23:33:47 +0100

    Create basicDistanceMeasureSerial.ino

---
+++ arduinoCode/basicDistanceMeasureSerial/basicDistanceMeasureSerial.ino
@@ -0,0 +1,49 @@
+/*
+    Ultrasonic sensor Pins:
+        VCC: +5VDC
+        Trig : Trigger (INPUT) - Pin11
+        Echo: Echo (OUTPUT) - Pin 12
+        GND: GND
+*/
+
+#define trigPin 11
+#define echoPin 12
+
+long duration;
+int cm;
+
+void setup() {
+  //Serial Port begin
+  Serial.begin (115200);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+
+void measureDistance(){
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  Serial.print(cm);
+  Serial.println();
+}
+
+void loop() {
+  measureDistance();
+  delay(50);
+}
