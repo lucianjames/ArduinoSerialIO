bda938cd8c1add0510bc2203236d064eb62beacb
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-07 23:59:58 +0100

    Debug log shit

--- arduinoSerialIO.hpp
+++ arduinoSerialIO.hpp
@@ -22,10 +22,12 @@ private:
     unsigned char buffer[BUFFERSIZE]; // Input buffer array, this array has to exist because its not possible to read into a vector
     std::vector<unsigned char> dataVect; // Vector to store the data from multiple reads into the buffer
     std::string ttyName; // Path to the serial port
+    bool debugLog = false; // Debug log flag
 public:
     // ===== Constructor/Destructor =====
-    ArduinoSerialIO(std::string tty){
+    ArduinoSerialIO(std::string tty, bool debug=false){
         this->ttyName = tty;
+        this->debugLog = debug;
         this->fd = open(this->ttyName.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
         if(this->fd == -1){
             throw std::runtime_error("Failed to open port " + this->ttyName);
@@ -46,7 +48,7 @@ public:
         options.c_cflag &= ~CSIZE;
         options.c_cflag |= CS8; // 8 bits
         options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // Raw input mode
-        sleep(1); // Give arduino a second to start up
+        //sleep(1); // Give arduino a second to start up
         tcflush(this->fd, TCIFLUSH); // Flush the input buffer
         std::cout << "Serial port " << this->ttyName << " opened\n";
     }
@@ -68,7 +70,7 @@ public:
             for(int i = 0; i < bytes_read; i++){
                 this->dataVect.push_back(this->buffer[i]); // Reading buffer[i] into the vector before checking if it is a newline will cause the vector to contain the newline character
                 if(this->buffer[i] == '\n'){
-                    std::cout << "Read " << bytes_read_total << " bytes from " + this->ttyName + "\n";
+                    if(debugLog){std::cout << "Read " << bytes_read_total << " bytes from " + this->ttyName + "\n";}
                     return;
                 }
             }
@@ -80,7 +82,7 @@ public:
             throw std::runtime_error("Failed to read from port " + this->ttyName);
             return;
         }
-        std::cout << "Read " << bytes_read << " bytes from " + this->ttyName << " (Should be 1, readChar() called)\n";
+        if(debugLog){std::cout << "Read " << bytes_read << " bytes from " + this->ttyName << " (Should be 1, readChar() called)\n";}
         this->dataVect.push_back(this->buffer[0]);
     }
 
@@ -92,7 +94,7 @@ public:
             throw std::runtime_error("Failed to write to port " + this->ttyName);
             return;
         }
-        std::cout << "Wrote " << bytes_written << " bytes to " << this->ttyName << '\n';
+        if(debugLog){std::cout << "Wrote " << bytes_written << " bytes to " << this->ttyName << '\n';}
     }
     void writeChar(char c){ // Writes a single character to the serial port
         int bytes_written = write(this->fd, &c, 1);
@@ -100,16 +102,17 @@ public:
             throw std::runtime_error("Failed to write to port " + this->ttyName);
             return;
         }
-        std::cout << "Wrote " << bytes_written << " bytes to " << this->ttyName << " (Should be 1, writeChar() called)\n";
+        if(debugLog){std::cout << "Wrote " << bytes_written << " bytes to " << this->ttyName << " (Should be 1, writeChar() called)\n";}
     }
 
 
     // ===== This->dataVect operations =====
     void clearDataVect(){ // Clears this->dataVect
         this->dataVect.clear();
-        std::cout << "Cleared this->dataVect\n";
+        if(debugLog){std::cout << "Cleared this->dataVect\n";}
     }
     std::vector<unsigned char> getDataVect(){ // Returns this->dataVect
+        if(debugLog){std::cout << "Returned this->dataVect\n";}
         return this->dataVect;
     }
     std::string getDataString(){ // Returns this->dataVect as a string
@@ -117,18 +120,22 @@ public:
         for(int i = 0; i < this->dataVect.size(); i++){
             str += this->dataVect[i];
         }
+        if(debugLog){std::cout << "Returned this->dataVect as a string of size " << str.length() << '\n';}
         return str;
     }
     int getDataInt(){ // Returns this->dataVect as an int, -1 if stoi() fails
         // Remove last character (assumed to be newline)
         std::string str = this->getDataString();
         if(str.length() < 2){
+            if(debugLog){std::cout << "getDataInt() failed, string too short (Assumed single newline)\n";}
             return -1;
         }
         str = str.substr(0, str.length() - 1);
         try{
+            if(debugLog){std::cout << "Returned this->dataVect as an int\n";}
             return std::stoi(str);
         }catch(...){
+            if(debugLog){std::cout << "Failed to convert this->dataVect to an int\n";}
             return -1;
         }
     }
@@ -137,5 +144,6 @@ public:
     // ===== Misc =====
     void tcflushFD(){ // Flushes /dev/ttyACM_ buffer
         tcflush(this->fd, TCIFLUSH);
+        if(debugLog){std::cout << "tcflushed " << this->ttyName << "\n";}
     }
 };
