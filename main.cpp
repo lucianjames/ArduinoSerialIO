00ea327fd1cdb4884809e20dad8a6493df7656ec
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-10 20:28:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,15 @@
 #include "arduinoSerial.h"
 
 int main(){
-    arduinoSerial serial("/dev/ttyACM0", true);
+    arduinoSerial serial("/dev/ttyACM0", false);
     serial.begin(B9600);
     serial.setTimeout(100000);
     while(true){
         if(serial.available() > 0){
-            std::cout << serial.readStringUntil('\n') << " <--- was read" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            std::cout << serial.readStringUntil('\n');
             serial.flush();
         }
-        // Wait for a bit
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     return 0;
 }
